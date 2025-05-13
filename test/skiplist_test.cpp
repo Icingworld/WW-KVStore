c70dc276164ce70b567459a5845e895bfe764e7c
icingworld <984442022@qq.com>
2025-05-13 14:15:20 +0800

    test: complete skiplist test

--- test/skiplist_test.cpp
+++ test/skiplist_test.cpp
@@ -27,7 +27,104 @@ TEST_F(SkipListTest, Insert)
     EXPECT_EQ(_Skiplist.size(), 3);
 
     pair = _Skiplist.insert({"a", "g"});
-    EXPECT_EQ(pair.first, _Skiplist.end());
+    EXPECT_EQ(pair.first->first, "a");
+    EXPECT_EQ(pair.first->second, "b");
     EXPECT_FALSE(pair.second);
     EXPECT_EQ(_Skiplist.size(), 3);
+}
+
+TEST_F(SkipListTest, Access)
+{
+    _Skiplist.insert({"a", "b"});
+    _Skiplist.insert({"c", "d"});
+    _Skiplist.insert({"e", "f"});
+
+    // at
+    EXPECT_EQ(_Skiplist.at("a"), "b");
+    EXPECT_EQ(_Skiplist.at("c"), "d");
+    EXPECT_EQ(_Skiplist.at("e"), "f");
+    EXPECT_THROW(_Skiplist.at("g"), std::out_of_range);
+
+    // operator[]
+    EXPECT_EQ(_Skiplist["a"], "b");
+    EXPECT_EQ(_Skiplist["c"], "d");
+    EXPECT_EQ(_Skiplist["e"], "f");
+    _Skiplist["a"] = "g";
+    EXPECT_EQ(_Skiplist["a"], "g");
+    _Skiplist["h"] = "i";
+    EXPECT_EQ(_Skiplist["h"], "i");
+}
+
+TEST_F(SkipListTest, Iterator)
+{
+    _Skiplist.insert({"a", "b"});
+    _Skiplist.insert({"c", "d"});
+    _Skiplist.insert({"e", "f"});
+    auto it = _Skiplist.begin();
+    EXPECT_EQ(it->first, "a");
+    EXPECT_EQ(it->second, "b");
+    ++it;
+    EXPECT_EQ(it->first, "c");
+    EXPECT_EQ(it->second, "d");
+    ++it;
+    EXPECT_EQ(it->first, "e");
+    EXPECT_EQ(it->second, "f");
+    ++it;
+    EXPECT_EQ(it, _Skiplist.end());
+}
+
+TEST_F(SkipListTest, Erase)
+{
+    _Skiplist.insert({"a", "b"});
+    _Skiplist.insert({"c", "d"});
+    _Skiplist.insert({"e", "f"});
+
+    // erase(1)
+    auto it = _Skiplist.begin();
+    ++it;
+    it = _Skiplist.erase(it);
+    EXPECT_EQ(it->first, "e");
+    EXPECT_EQ(it->second, "f");
+    EXPECT_EQ(_Skiplist.size(), 2);
+
+    // erase(2)
+    auto count = _Skiplist.erase("e");
+    EXPECT_EQ(count, 1);
+    EXPECT_EQ(_Skiplist.size(), 1);
+
+    count = _Skiplist.erase("g");
+    EXPECT_EQ(count, 0);
+    EXPECT_EQ(_Skiplist.size(), 1);
+}
+
+TEST_F(SkipListTest, Clear)
+{
+    _Skiplist.insert({"a", "b"});
+    _Skiplist.insert({"c", "d"});
+    _Skiplist.insert({"e", "f"});
+    _Skiplist.clear();
+    EXPECT_EQ(_Skiplist.size(), 0);
+    EXPECT_TRUE(_Skiplist.empty());
+
+    _Skiplist.insert({"a", "b"});
+    _Skiplist.insert({"c", "d"});
+    _Skiplist.insert({"e", "f"});
+    EXPECT_EQ(_Skiplist.size(), 3);
+    EXPECT_FALSE(_Skiplist.empty());
+}
+
+TEST_F(SkipListTest, FindAndContains)
+{
+    _Skiplist.insert({"a", "b"});
+    _Skiplist.insert({"c", "d"});
+    _Skiplist.insert({"e", "f"});
+    auto it = _Skiplist.find("a");
+    EXPECT_EQ(it->first, "a");
+    EXPECT_EQ(it->second, "b");
+    it = _Skiplist.find("g");
+    EXPECT_EQ(it, _Skiplist.end());
+    EXPECT_TRUE(_Skiplist.contains("a"));
+    EXPECT_TRUE(_Skiplist.contains("c"));
+    EXPECT_TRUE(_Skiplist.contains("e"));
+    EXPECT_FALSE(_Skiplist.contains("g"));
 }
